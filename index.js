7b6686aa06110a310c5f9c0f554afc98137d8e12
Anandkumar NS <69028746+aknsubbu@users.noreply.github.com>
2023-04-10 11:47:13 +0530

    Add files via upload
    Added my version of the code

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-import app from './server.js'
-import mongodb from "mongodb"
-import Attedance from "./server/attendance.js"
+const express=require('express');
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
 
-const MongoClient = mongodb.MongoClient
-const uri = "mongodb+srv://akash:v3621O59VW8g0BNb@psgscapes-data-ac295d1f.mongo.ondigitalocean.com/?authMechanism=DEFAULT"
+//set up express app
+const app=express();
 
-const port = 8000
+//setting up body-parser
+app.use(bodyParser.json());
 
-MongoClient.connect(
-    uri,
-    {
-        maxPoolSize: 50,
-        wtimeoutMS: 2500,
-        useNewUrlParser: true
-    })
-    .catch(err => {
-        console.error(err.stack)
-        process.exit(1)
-    })
-    .then(async client => {
-        await Attedance.injectDB(client)
-        app.listen(port, () => {
-            console.log(`listening on port ${port}`)
-        })
-    } )
+// setting routes folder
+app.use('/api',require('./routes/api'));
+
+//connect to mongodb
+//mongodb+srv://<username>:<password>@cluster0.pfose.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect('mongodb+srv://anandkumar:05KnDt9g1R462f3h@psgscapes-data-ac295d1f.mongo.ondigitalocean.com/attendance?tls=true&authSource=admin&replicaSet=psgscapes-data')
+mongoose.Promise=global.Promise;
+
+//listening for requests 
+app.listen(process.env.port || 4000,function(){
+    console.log('now listening for requests');
+});
+
+//error handling 
+app.use(function(err,req,res,next){
+    res.status(422).send({error:err.message});
+});
