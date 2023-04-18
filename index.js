dd0104628151d54d0e81449238b3bef56626b781
aknsubbu <aknsubbu@gmail.com>
2023-04-18 12:12:13 +0530

    new comms

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const mongoose=require('mongoose');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 //set up express app
-const app=express();
+const app = express();
 
 //setting up body-parser
 app.use(bodyParser.json());
-app.use(morgan())
+app.use(morgan("dev"));
 // setting routes folder
-app.use('/api',require('./routes/api'));
+app.use("/api", require("./routes/api"));
 
 //connect to mongodb
 //mongodb+srv://<username>:<password>@cluster0.pfose.mongodb.net/<dbname>?retryWrites=true&w=majority
-mongoose.connect('mongodb+srv://anandkumar:05KnDt9g1R462f3h@psgscapes-data-ac295d1f.mongo.ondigitalocean.com/attendance?tls=true&authSource=admin&replicaSet=psgscapes-data')
-mongoose.Promise=global.Promise;
+mongoose.connect(
+  "mongodb+srv://anandkumar:05KnDt9g1R462f3h@psgscapes-data-ac295d1f.mongo.ondigitalocean.com/attendance?tls=true&authSource=admin&replicaSet=psgscapes-data"
+);
+mongoose.Promise = global.Promise;
 
-//listening for requests 
-app.listen(process.env.port || 4000,function(){
-    console.log('now listening for requests');
+//listening for requests
+app.listen(process.env.port || 4000, function () {
+  console.log("Server Live for requests on PORT 4000");
 });
 
-//error handling 
-app.use(function(err,req,res,next){
-    res.status(422).send({error:err.message});
+//error handling
+app.use(function (err, req, res, next) {
+  res.status(422).send({ error: err.message });
 });
