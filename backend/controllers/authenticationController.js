c8d873e41e972ab7647d0b57bf44c714a11d3432
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-05-27 03:46:08 +0000

    modified verify route to send userinfo

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -11,7 +11,9 @@ const credential = async (req, res) => {
 const verify = asyncHandler(async (req, res) => {
   const userInfo = await user.findOne({ _id: req.body.rollnumber });
   if (userInfo["password"] == req.body.password) {
-    delete userInfo["password"]
+    console.log(userInfo)
+    userInfo.password = undefined
+    console.log(userInfo)
     res.status(200).json({userInfo});
   } else {
     res.status(400).json({ status: false });
