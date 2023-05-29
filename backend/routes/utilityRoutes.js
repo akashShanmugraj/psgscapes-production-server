2a03100d43f3ef5a4d1cd95328eb5ff5f9a0b999
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-05-29 04:05:13 +0000

    added /utility/info route

--- backend/routes/utilityRoutes.js
+++ backend/routes/utilityRoutes.js
@@ -14,6 +14,25 @@ router.get("/course/all", courseinfo);
 router.get("/attendance/all", attendanceinfo);
 router.get("/user/all", userinfo);
 router.get("/help", utilityhelp);
+router.post("/info", (req, res) => {
+  const requestObject = {
+    "req.app": req.app,
+    "req.baseurl": req.baseUrl,
+    "req.body": req.body,
+    "req.cookie": req.cookie,
+    "req.hostname": req.hostname,
+    "req.ipv6": req.ipv6,
+    "req.params": req.params,
+    "req.path": req.path,
+    "req.protocol": req.protocol,
+    "req.query": req.query,
+    "req.route": req.route,
+    "req.tlsInsecure": req.tlsInsecure,
+    "req.auth": req.auth,
+  };
+
+  res.send(requestObject);
+})
 router.get("/", (req, res) => {
     res.send("these are utility routes, kindly go to /utility/help for more information");
 });
